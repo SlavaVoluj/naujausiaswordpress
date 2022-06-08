c80cb13d6ad04c22836e9d97d43c497ae521753a
domsius <dominykas.luksys@gmail.com>
2022-06-08 14:53:33 +0300

    fixed some paths

---
+++ themes/ManoNaujasProjektas/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <!-- Nav block -->
+    <div class="navigation">
+        <nav class="navbar navbar-expand-lg .text-white ">
+            <div class="container">
+                <a href="#"><img src="icons/Logo malex.png"></a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+                            <a class="nav-link active " aria-current="page" href="services.html">SERVICES</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="aboutUs.html">ABOUT US</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="team.html">TEAM</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="pricing.html">PRICING</a>
+                        </li>
+
+
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="news.html">NEWS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#contacts">CONTACTS</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
--- themes/ManoNaujasProjektas/index.php
+++ themes/ManoNaujasProjektas/index.php
@@ -1,4 +1,52 @@
-<?php get_header(); ?> 
+<body>
+
+    <!-- Nav block -->
+    <div class="navigation">
+
+        <nav class="navbar navbar-expand-lg .text-white ">
+            <div class="container">
+                <a href="#"><img src="icons/Logo malex.png"></a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+                            <a class="nav-link active " aria-current="page" href="services.html">SERVICES</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="aboutUs.html">ABOUT US</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="team.html">TEAM</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="pricing.html">PRICING</a>
+                        </li>
+
+                        
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="news.html">NEWS</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#contacts">CONTACTS</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
         <!-- Hero block -->
         <div class="wrapper h-100">
             <div class="container h-100">
@@ -741,4 +789,28 @@
 
 
 
-<?php get_footer(); ?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
